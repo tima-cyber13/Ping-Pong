e26e631710be67fe5301db92e204d94baa0d51fe
tima-cyber13 <timovey.kvach@kischool.ru>
2026-04-19 13:34:53 +0300

    Update main.py
    добавил класс Player и описал его функции для ракеток с учетом разных сторон 
    
    также добавил и заполнил задний фон игры.

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+from pygame import *
 
+class Player(GameSprite):
+    def update_R(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y >5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.x < win_width - 80:
+            self.rect.y += self.speed
+    
+    def update_L(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y >5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.x < win_width - 80:
+            self.rect.y += self.speed
+
+
+back = (200,255,255)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
