445c6029b6f642ff27d63bf442cc538a4a9ef2a1
tima-cyber13 <timovey.kvach@kischool.ru>
2026-05-03 12:46:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 from pygame import *
+from random import randint
+
+
+class GameSprite(sprite.Sprite):
+   def __init__(self, player_image, player_x, player_y, player_speed, scale_x=65, scale_y=65):
+       super().__init__()
+       self.image = transform.scale(image.load(player_image), (scale_x, scale_y))
+       self.speed = player_speed
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+
 
 class Player(GameSprite):
-    def update_R(self):
+    def update_r(self):
         keys = key.get_pressed()
-        if keys[K_UP] and self.rect.y >5:
+        if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.x < win_width - 80:
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
-    
-    def update_L(self):
+    def update_l(self):
         keys = key.get_pressed()
-        if keys[K_w] and self.rect.y >5:
+        if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_s] and self.rect.x < win_width - 80:
+        if keys[K_s] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
-
-
-back = (200,255,255)
-win_width = 600
-win_height = 500
-window = display.set_mode((win_width, win_height))
-window.fill(back)
